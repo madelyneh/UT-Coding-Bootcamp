1f1e77d5f37875ece60ca43ef80431661bda15ad
Madi <madelyn.e.helton@gmail.com>
2019-02-27 19:01:29 -0600

    adding new files

---
+++ 19-February/02202019/last-unsolved/Pokemon.js
@@ -0,0 +1,48 @@
+class Pokemon {
+  constructor(name) {
+    this.name = name;
+    this.isSleepy = true;
+    this.isHungry = false;
+  }
+
+  sayName() {
+    console.log(this.name + "!");
+  }
+
+  eat() {
+    if (this.isHungry) {
+      this.isHungry = false;
+      this.isSleepy = true;
+      console.log(
+        this.name + " finished eating, " + this.name + " is sleepy now."
+      );
+    } else {
+      console.log(
+        this.name + " isn't hungry. " + this.name + " is sleepy though."
+      );
+    }
+  }
+
+  sleep() {
+    if (this.isSleepy) {
+      this.isSleepy = false;
+      this.isHungry = true;
+      console.log(this.name + " went to sleep...");
+      console.log(this.name + " woke up refreshed! And hungry!");
+    } else {
+      console.log(
+        this.name + " isn't sleepy. " + this.name + " is hungry though."
+      );
+    }
+  }
+};
+
+const newPikachu = new Pokemon("Pikachu");
+
+newPikachu.sayName();
+newPikachu.sleep();
+newPikachu.eat();
+
+export {Pokemon}; 
+
+export default Pokemon;
